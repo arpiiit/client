c19a04a6ec86fca3314631f4429c33b83b9ccc87
Arpit Mishra <arpitmishra652003@gmail.com>
2024-05-06 19:38:39 +0530

    added routing features

---
+++ src/pages/ArticalList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ArticalList = () => {
+  return (
+    <div>
+          <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">Articals</h1>
+
+    </div>
+  )
+}
+
+export default ArticalList
---
+++ src/pages/Home.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">Hello, Wellcome to my Blog</h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Every time you write a blog post, you want your readers to take action.
+        This action may vary depending on the overall purpose of your blog and
+        the specific subject of each individual post. Your goal may be to get
+        your reader to subscribe to your blog, to share your post with others,
+        to purchase a product, make healthier life decisions, or change their
+        views on a particular matter. The possibilities are endless. If you use
+        power words in an effective way, you can increase the likelihood that
+        your call to action, whatever it may be, will be followed.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        It takes a bit of practice to learn the most effective ways to include
+        power words in your blog posts. If you use too many power words or try
+        to evoke an inappropriate level of emotion on a particular topic, that
+        can be troublesome for your readers. This is because your writing may
+        come off as, emotionally manipulative, overwrought, histrionic, and
+        phony. On the other hand, if you don’t use enough power words, or you do
+        not take advantage of opportunities to really ‘hit your audience in the
+        feels’ when it matters, you may as well ignore power words altogether.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        It takes a bit of practice to learn the most effective ways to include
+        power words in your blog posts. If you use too many power words or try
+        to evoke an inappropriate level of emotion on a particular topic, that
+        can be troublesome for your readers. This is because your writing may
+        come off as, emotionally manipulative, overwrought, histrionic, and
+        phony. On the other hand, if you don’t use enough power words, or you do
+        not take advantage of opportunities to really ‘hit your audience in the
+        feels’ when it matters, you may as well ignore power words altogether.
+        Two other things that you will need to take into consideration are your
+        target audience and your writing style. You don’t want to give your
+        audience mental whiplash, for example, by using power words in a way
+        that completely changes your writing style. You also don’t want to
+        change the tone of your blog in such a way that it puts off your current
+        audience. Your use of power words should complement your writing style,
+        and appeal to your target demographic. Whatever you do, be sure you
+        always use power words correctly. Writing misused words will not get the
+        reaction you intend.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
